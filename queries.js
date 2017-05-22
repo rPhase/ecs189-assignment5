db2d6659eaae535b29019a7c5a57a35b3831851f
Ryan <rphuang@ucdavis.edu>
2017-05-22 00:56:11 -0700

    add delete

--- queries.js
+++ queries.js
@@ -7,7 +7,6 @@ var sqlite3 = require("sqlite3").verbose();  // use sqlite
 var dbFile = "photos.db"
 var db = new sqlite3.Database(dbFile);  // new object, old DB
 
-
 // SERVER CODE
 // Handle request to add a label
 var querystring = require('querystring'); // handy for parsing query strings
@@ -15,6 +14,7 @@ var querystring = require('querystring'); // handy for parsing query strings
 function answer(query, response) {
 	// query looks like: op=add&img=[image filename]&label=[label to add]
 	queryObj = querystring.parse(query);
+//==============================================================================
   if (queryObj.op == "add") {
 		var newLabel = queryObj.label;
 		var imageFile = queryObj.img;
@@ -47,6 +47,68 @@ function answer(query, response) {
 							labelString = newLabel;
 						} else {
 							labelString = data.labels + ", " + newLabel;
+						}
+						console.log(labelString);
+						db.run(
+							'UPDATE photoLabels SET labels = ? WHERE fileName = ?',
+							[labelString, imageFile],
+							updateCallback);
+						}
+				}
+	    }
+
+			// Also define this inside queries so it knows about
+			// response object
+			function updateCallback(err) {
+				console.log("updating labels for "+imageFile+"\n");
+				if (err) {
+					console.log(err+"\n");
+					sendCode(400,response,"requested photo not found");
+				} else {
+					// send a nice response back to browser
+					response.status(200);
+					response.type("text/plain");
+					response.send("added label "+newLabel+" to "+imageFile);
+				}
+	    }
+		}
+	}
+//==============================================================================
+	if (queryObj.op == "delete") {
+		var newLabel = queryObj.label;
+		var imageFile = queryObj.img;
+		if (newLabel && imageFile) {
+			// good delete query
+			// go to database!
+			db.get(
+				'SELECT labels FROM photoLabels WHERE fileName = ?',
+				[imageFile], getCallback);
+
+			// define callback inside queries so it knows about imageFile
+			// because closure!
+	    function getCallback(err,data) {
+				console.log("getting labels from "+imageFile);
+				// console.log(data);
+				var arrayLabel = data.labels.split(", ");
+				var indexLabel = arrayLabel.indexOf(newLabel);
+				if (indexLabel==-1) {
+					console.log("label does not exist\n");
+					response.status(200);
+					response.type("text/plain");
+					response.send("label "+newLabel+" does not exist");
+				}
+				else {
+					if (err) {
+						console.log("error: ",err,"\n");
+					} else {
+						// good response...so let's update labels
+						arrayLabel = arrayLabel.splice(indexLabel, 1);
+						var labelString;
+						for (int i = 0; i < arrayLabel.length; i++) {
+							labelString = labelString + arrayLabel[i];
+							if (i != arrayLabel.length-1) {
+								labelString = labelString + ", ";
+							}
 						}
 						console.log(labelString);
 						db.run(
